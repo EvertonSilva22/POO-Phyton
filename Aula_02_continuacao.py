e360003bbc0ad3131a52293f4e6a7f4a125fc38b
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-09 23:53:38 -0300

    Aprendendo a multiplicar valores.

--- Aula_02_continuacao.py
+++ Aula_02_continuacao.py
@@ -5,7 +5,12 @@ def triplicar(x):
     triplo = 3 * x
     return triplo
 
+def quatro(x):
+    quatro = 4 * x
+    return quatro
+
 n1 = float(input('Digite um número: '))  
 dobro = dobrar(n1)
 print(f'O dobro é: {dobro}')
-print(f'O triplo é: {triplicar(n1)}')      
+print(f'O triplo é: {triplicar(n1)}') 
+print(f'O Quádruplo é: {quatro(n1)}')     
