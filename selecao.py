134c664a699b33237b50b732f62fcc610b1cb7c2
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-11 23:54:02 -0300

    introdução de condições.
    aprendendo if e else.

--- selecao.py
+++ selecao.py
@@ -1,4 +1,11 @@
-n = int(int('Digite um número:  '))
+n = int(input('Digite um número:  '))
 
 if n > 10:
     print('n é maior que 10')
+else:
+    if n== 10:
+        print('n é igual a 10')
+    else:
+        print('n é menor a 10')
+
+print('fim')
