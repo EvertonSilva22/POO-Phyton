bdcf8c426757a51edc12f46e91b05d08d76365ec
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-11 23:59:41 -0300

    introdução.
    Aprendendo como utilizar o Elif.

--- selecao.py
+++ selecao.py
@@ -9,3 +9,15 @@ else:
         print('n é menor a 10')
 
 print('fim')
+
+#ou então utilizando elif
+n = int(input('Digite um número:  '))
+
+if n > 10:
+    print('n é maior que 10')
+elif n== 10:
+    print('n é igual a 10')
+else:
+    print('n é menor que 10')
+
+print('fim')
