16b76ec88c1304b8728e491a73567322ed8a7266
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-12 23:09:38 -0300

    fim das estruturas de repetição.

---
+++ repeticao.py
@@ -0,0 +1,27 @@
+cont = 0
+while cont< 5:
+    print(cont)
+    cont += 1
+
+print('fim while')
+
+for cont2 in range(5):
+    print(cont2)
+
+print('fim for')  
+
+#a função FOR seria o mesmo que a função abaixo.
+
+for cont2 in[0, 1, 2, 3, 4]:
+    print(cont2)
+
+print('fim for')    
+
+#contagem de uma numeração definida.
+
+n = int(input('Digite um número: '))
+
+for cont2 in range(n):
+    print(cont2)
+
+print('fim for')
