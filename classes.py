86550dbb4098061805cc04bcc011894138037b3e
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-17 23:13:34 -0300

    Criando minha primeira classe.
    utilizando a classe "Livro".

--- classes.py
+++ classes.py
@@ -6,3 +6,15 @@ objeto_1 = NomeDaClasse()
 objeto_2 = NomeDaClasse()
 
 objeto_1.atributo_1 = 'Valor do atributo 1'
+
+##########################################
+class Livro:
+    def __init__(self):
+     self.atributo_1= 'valor1'
+     self.atributo_2= 'valor2'
+
+livro_1 = Livro()
+livro_2 = Livro()
+
+print('livro 1:', vars(livro_1))
+print('livro 2:',vars(livro_2))
