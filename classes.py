d896560feace5b42137364237a5025ebf6bb4e37
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-18 22:51:13 -0300

    Classes.
    Trabalhando a classe livro.

--- classes.py
+++ classes.py
@@ -9,12 +9,17 @@ objeto_1.atributo_1 = 'Valor do atributo 1'
 
 ##########################################
 class Livro:
-    def __init__(self):
-     self.atributo_1= 'valor1'
-     self.atributo_2= 'valor2'
+    def __init__(self, nome, autor):
+     self.nome= nome
+     self.autor= autor
+     self.editora= 'Nome da editora'
 
-livro_1 = Livro()
-livro_2 = Livro()
+livro_1 = Livro('O cão dos Baskeville', 'Everton Silva')
+livro_2 = Livro('O Príncipe', 'Nicolau Maquiavel')
 
 print('livro 1:', vars(livro_1))
-print('livro 2:',vars(livro_2))
+print(livro_1.nome)
+print(livro_1.autor)
+
+
+#print('livro 2:',vars(livro_2))#
