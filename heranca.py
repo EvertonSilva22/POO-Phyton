20a657e81f95036eac04bdb49c0e42f27d9f3947
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-22 00:09:07 -0300

    Herança.
    Passando heranças.

--- heranca.py
+++ heranca.py
@@ -1,12 +1,17 @@
 class Mae:  # Mamifero
     def __init__(self, p1):
         print('executando o init de Mãe')
+        self.p1 = p1
 
 class Filha(Mae):  # Felino
      def __init__(self, p1, p2):
         print('executando o init de Filha')
-
+        self.p1 = p1
+        self.p2 = p2
 
 class Neta(Filha):  # Gato
     def __init__(self, p1, p2, p3):
         print('executando o init de Neta') 
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
