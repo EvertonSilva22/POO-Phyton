5fa233da34af0125a8cb92315f8bc63f69e4f1f7
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-22 00:11:41 -0300

    Herança.
    Forma mais pratica de passar heranças.

--- heranca.py
+++ heranca.py
@@ -6,12 +6,11 @@ class Mae:  # Mamifero
 class Filha(Mae):  # Felino
      def __init__(self, p1, p2):
         print('executando o init de Filha')
-        self.p1 = p1
         self.p2 = p2
+        Mae.__init__(self, p1)
 
 class Neta(Filha):  # Gato
     def __init__(self, p1, p2, p3):
         print('executando o init de Neta') 
-        self.p1 = p1
-        self.p2 = p2
         self.p3 = p3
+        Filha.__init__(self, p1, p2)
