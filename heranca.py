33c51f5dd24a1be5f8799566d8859e71ad5d8ed6
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-21 23:58:52 -0300

    Heranças.
    Atribuindo valores.

--- heranca.py
+++ heranca.py
@@ -1,12 +1,12 @@
 class Mae:  # Mamifero
-    def __init__(self):
+    def __init__(self, p1):
         print('executando o init de Mãe')
 
 class Filha(Mae):  # Felino
-     def __init__(self):
+     def __init__(self, p1, p2):
         print('executando o init de Filha')
 
 
 class Neta(Filha):  # Gato
-    def __init__(self):
+    def __init__(self, p1, p2, p3):
         print('executando o init de Neta') 
