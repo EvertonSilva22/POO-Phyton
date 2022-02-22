389d2495c3be747a1bd6a8abc0bec6e4ecb2854e
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-22 00:24:53 -0300

    Herança.
    Classes em herança.

--- teste_heranca.py
+++ teste_heranca.py
@@ -1,11 +1,16 @@
 from heranca import Mae, Filha, Neta
 
 print('Criando objeto de Mae...')
-mae = Mae()
+mae = Mae(1)
 
 print('\n\n criando objeto de Filha...')
-filha = Filha()
+filha = Filha(1, 2)
 
 
 print('\n\n criando objeto de Neta...')
-neta = Neta()
+neta = Neta(1, 2, 3)
+
+print('\n Visualizando os objetos')
+print('mae', vars(mae))
+print('filha', vars(filha))
+print('neta', vars(neta))
