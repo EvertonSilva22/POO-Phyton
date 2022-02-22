ae41cb77e715160aa7279c6f5ddce411b2e63e5f
EvertonSilva22 <86390461+EvertonSilva22@users.noreply.github.com>
2022-02-21 23:55:45 -0300

    Herança.
    Criando Objetos para herança.

--- teste_heranca.py
+++ teste_heranca.py
@@ -1,6 +1,11 @@
 from heranca import Mae, Filha, Neta
 
-
+print('Criando objeto de Mae...')
 mae = Mae()
+
+print('\n\n criando objeto de Filha...')
 filha = Filha()
+
+
+print('\n\n criando objeto de Neta...')
 neta = Neta()
